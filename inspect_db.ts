5fde2e2702904d9a76fe9b8c5141be65baf00fb4
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2026-04-06 21:01:47 +0000

    chore: remove leftover script

--- inspect_db.ts
+++
@@ -1,9 +0,0 @@
-import { openDb } from './packages/engine/src/db/db.ts';
-const db = openDb('./demo/data/associations-demo/state.db');
-console.log('=== deferred_associations ===');
-console.log(JSON.stringify(db.prepare('SELECT * FROM deferred_associations').all(), null, 2));
-console.log('=== watermarks ===');
-console.log(JSON.stringify(db.prepare('SELECT * FROM watermarks').all(), null, 2));
-const ids = db.prepare("SELECT * FROM identity_map WHERE external_id IN ('c4','co4') OR canonical_id IN (SELECT canonical_id FROM identity_map WHERE external_id IN ('c4','co4'))").all();
-console.log('=== identity_map for c4/co4 ===');
-console.log(JSON.stringify(ids, null, 2));
